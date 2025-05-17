1662f50d07d140ce9f5ed2dc419dfe67e5d969bb
DevMartins <h.martins.silva2014@gmail.com>
2025-05-17 17:05:22 -0300

    feat: Page style main

--- src/components/main/Index.jsx
+++ src/components/main/Index.jsx
@@ -1,15 +1,21 @@
-
+import style from './Index.module.css'
+import carrinho from '../../images/main/carrinho-de-compras.png'
 
 export default function Index(){
 
     return(
-        <main>
-            <section>
-                <article>
-                    <h2>Estoquista <br></br>Atakado</h2>
+        <main className={style.main}>
+            <section className={style.section}>
+                <article className={style.article}>
+                    <div>
+                        <h2 className={style.h1}>Estoquista <br/>Atakado</h2>
+                    </div>
+                    <div className={style.article_Titulo2}>
+                        <h3>Comércio LTDA</h3>
+                    </div>  
                 </article>
-                <figure>
-
+                <figure className={style.figure}>
+                    <img className={style.imagens} src={carrinho} alt="Carrinho de Supermercado" />
                 </figure>
             </section>
         </main>
