11fa8e6778e4199ab1ace00458104aa38d1f5c65
DevMartins <h.martins.silva2014@gmail.com>
2025-05-19 15:29:28 -0300

    Feat & Style: Add Stock Page

--- src/pages/Administration/administration.jsx
+++ src/pages/Administration/administration.jsx
@@ -1,38 +1,41 @@
 import Header from '../../components/header/Index';
 import ImgCarrinho from '../../images/main/carrinho-de-compras.png'
 import Button from '../../components/button/Index'
+import Style from './administration.module.css'
 
 export default function administration(){
 
     return(
-        <>
-            <header>
-                <nav>
-                    <div>
-                        <figure>
-                            <img src={ImgCarrinho} alt="Carinho" />
+        <div className={Style.container}>
+            <header className={Style.header}>
+                <nav className={Style.nav}>
+                    <div className={Style.information}>
+                        <figure className={Style.figure}>
+                            <img className={Style.imagem} src={ImgCarrinho} alt="Carinho" />
                         </figure>
-                        <div>
-                            <p>Nome da Loja</p>
+                        <div className={Style.store}>
+                            <p>Atakado</p>
                         </div>
                     </div>
-                    <div>
+                    <div className={Style.service}>
+                        <p>Serviços</p>
+                    </div>
+                    <div className={Style.button}>
                         <Button title="Home"/>
                         <Button title="Estoque"/>
                     </div>
-
                 </nav>
+                <footer className={Style.footer}>
+                    <div >
+                        <h2 className={Style.footer_titule}>Atakado</h2>
+                    </div>
+                </footer>
             </header>
-            <main>
+            <main className={Style.main}>
                 <section>
                     <h2>Conteúdo</h2>
                 </section>
             </main>
-            <footer>
-                <div>
-                    <h2>Atakado</h2>
-                </div>
-            </footer>
-        </>
+        </div>
     )
 }
